1b0030ceeeae64c306ef790722062c50e06032df
jeanlouis <lauret.jlouis@gmail.com>
2017-03-08 16:50:30 +0100

    Ajout de tweet.nb_last

--- src/AppBundle/Controller/TweetController.php
+++ src/AppBundle/Controller/TweetController.php
@@ -15,7 +15,9 @@ class TweetController extends Controller
      */
     public function listAction(Request $request)
     {
-        $tweets = $this->getDoctrine()->getRepository(Tweet::class)->getLastTweets(5);
+        $tweets = $this->getDoctrine()->getRepository(Tweet::class)->getLastTweets(
+            $this->getParameter('app.tweet.nb_last',10)
+        );
         return $this->render(':tweet:list.html.twig', [
             "tweets" => $tweets,
         ]);
